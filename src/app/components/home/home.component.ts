88e13460d662694dbb6ad83107e8bdfa56f2875b
zk652 <zhikangliu1234@gmail.com>
2024-05-02 11:00:44 -0700

    move navbar to app component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,23 +6,7 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private router: Router, public authService: AuthService) {}
+  constructor() {}
 
   ngOnInit(): void {}
-
-  get username(): string | null {
-    return this.authService.getUsername();
-  }
-
-  toLogin() {
-    this.router.navigate(['/login']);
-  }
-
-  logout() {
-    // Remove the token and username from local storage
-    localStorage.removeItem('authToken');
-    localStorage.removeItem('username');
-    // redirect the user to the login page
-    this.router.navigate(['/login']);
-  }
 }
