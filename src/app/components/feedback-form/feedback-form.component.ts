f4158976a67c893c95e9b05667674f4a9463db38
gabecpf97 <gabrielcpf2997@gmail.com>
2024-05-01 21:56:26 -0700

    added form for visa and visa interface

---
+++ src/app/components/feedback-form/feedback-form.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-feedback-form',
+  templateUrl: './feedback-form.component.html',
+  styleUrls: ['./feedback-form.component.css'],
+})
+export class FeedbackFormComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      status: ['', Validators.required],
+      feedback: ['', Validators.required],
+    });
+  }
+
+  handleSubmit = (): void => {
+    console.log('Form submitted:', this.form.value);
+  };
+
+  ngOnInit(): void {}
+}
