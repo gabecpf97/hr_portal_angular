fb731632033092cecbfb5af8a02785c45a880682
zk652 <zhikangliu1234@gmail.com>
2024-05-02 14:38:10 -0700

    handle token expires

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   private loginUrl = 'http://127.0.0.1:3000/auth/login';
 
@@ -20,5 +21,13 @@ export class AuthService {
 
   getUsername(): string | null {
     return localStorage.getItem('username');
+  }
+
+  logout(): void {
+    // Remove the token and username from local storage
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('username');
+    // redirect the user to the login page
+    this.router.navigate(['/login']);
   }
 }
