5b1ec59e4abc82ea1710874d046f011b8eba5b9d
gabecpf97 <gabrielcpf2997@gmail.com>
2024-05-04 17:01:38 -0700

    added updating to feedback on visa

--- src/app/components/feedback-form/feedback-form.component.ts
+++ src/app/components/feedback-form/feedback-form.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { catchError } from 'rxjs';
+import { visa } from 'src/app/interfaces/visa';
+import { visaActions } from 'src/app/store/visa/visa.actions';
 
 @Component({
   selector: 'app-feedback-form',
@@ -7,9 +12,15 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./feedback-form.component.css'],
 })
 export class FeedbackFormComponent implements OnInit {
+  @Input() id: string;
   form: FormGroup;
+  private url = 'http://localhost:3000/visa';
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private store: Store
+  ) {
     this.form = this.fb.group({
       status: ['', Validators.required],
       feedback: ['', Validators.required],
@@ -18,7 +29,22 @@ export class FeedbackFormComponent implements OnInit {
 
   handleSubmit = (): void => {
     console.log('Form submitted:', this.form.value);
+    this.http
+      .put<any>(`${this.url}/${this.id}/action`, this.form.value)
+      .pipe(
+        catchError((err) => {
+          console.log('error in updating visa', this.id, err);
+          return '';
+        })
+      )
+      .subscribe((response: { visa: visa }) => {
+        if (response && response.visa) {
+          this.store.dispatch(visaActions.update({ newVisa: response.visa }));
+        }
+      });
   };
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.id);
+  }
 }
